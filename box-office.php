3d827bcc8a868611d58f07a162d0e2e632298202
hattmarris <hattmarris@users.noreply.github.com>
2014-12-02 00:51:10 -0800

    Update box-office.php

--- box-office.php
+++ box-office.php
@@ -7,7 +7,7 @@
 * Version: 1.0.0
 * Author: Name of the plugin author
 * Text Domain: box-office 
-* License: A short license name. Example: GPL2
+* License: GPL2
 */
 
 defined('ABSPATH') or die("No script kiddies please!");
