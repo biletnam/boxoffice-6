96a198c2049f3d05cbafab0b2e985dcbae73b720
hattmarris <mattharris89@gmail.com>
2014-12-02 00:49:54 -0800

    additional info to box-office.php header

--- box-office.php
+++ box-office.php
@@ -1,11 +1,13 @@
 <?php
 /**
 *
-* Box Office Plugin for Wordpress
-* 
-* @author: Matt Harris
-* @package: box-office
-* 	
+* Plugin Name: Box Office  	
+*  
+* Description: A Box Office Plugin for WordPress
+* Version: 1.0.0
+* Author: Name of the plugin author
+* Text Domain: box-office 
+* License: A short license name. Example: GPL2
 */
 
 defined('ABSPATH') or die("No script kiddies please!");
